7d3d25f60abdf7511e315f932c68e17c1550735d
Quang <nmquang2303@gmail.com>
2025-05-07 16:37:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link rel="stylesheet" href="/assets/css/css-reset.css" />
+  <link rel="stylesheet" href="/assets/css/header.css" />
+</head>
+
+<body>
+  <header class="medpro-header">
+    <?php include 'layouts/components/header.php'; ?>
+  </header>
+
+  <main></main>
+
+  <footer></footer>
+</body>
+
+</html>
