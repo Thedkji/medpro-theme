0ed8bac0710b3f3eead1738e8d2e8a727cfb2e75
Quang <nmquang2303@gmail.com>
2025-05-16 17:27:08 +0700

    [quang] : hoàn thiện trang chủ 80% màn desktop

---
+++ resources/views/components/carousels/carousel7.blade.php
@@ -0,0 +1,125 @@
+@push('styles')
+    <link rel="stylesheet" href="./assets/css/home/carousels/carousel7.css">
+@endpush
+@push('scripts')
+    <script src="./assets/js/home/carousels/carousel7.js"></script>
+@endpush
+<div class="carousel7 relative">
+    <x-title.title2 title="Cảm nhận từ khách hàng" />
+
+    <div class="swiper">
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">
+                <div>
+                    <div class="img">
+                        <img src="./assets/imgs/Icon.svg" alt="">
+                        <div>
+                            <span>
+                                Đặt lịch xét nghiệm bên này rất gọn, có ngày giờ cụ thể luôn lên là được xét nghiệm
+                                liền
+                                không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                            </span>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <img src="./assets/imgs/d86082a5-b561-4904-a6c5-0122d8965ba9-nhan-nguyen.webp" alt="">
+                        <div class="name">
+                            <span>Nhân Nguyễn</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div>
+                    <div class="img">
+                        <img src="./assets/imgs/Icon.svg" alt="">
+                        <div>
+                            <span>
+                                Đặt lịch xét nghiệm bên này rất gọn, có ngày giờ cụ thể luôn lên là được xét nghiệm
+                                liền
+                                không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                            </span>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <img src="./assets/imgs/d86082a5-b561-4904-a6c5-0122d8965ba9-nhan-nguyen.webp" alt="">
+                        <div class="name">
+                            <span>Nhân Nguyễn</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div>
+                    <div class="img">
+                        <img src="./assets/imgs/Icon.svg" alt="">
+                        <div>
+                            <span>
+                                Đặt lịch xét nghiệm bên này rất gọn, có ngày giờ cụ thể luôn lên là được xét nghiệm
+                                liền
+                                không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                            </span>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <img src="./assets/imgs/d86082a5-b561-4904-a6c5-0122d8965ba9-nhan-nguyen.webp" alt="">
+                        <div class="name">
+                            <span>Nhân Nguyễn</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div>
+                    <div class="img">
+                        <img src="./assets/imgs/Icon.svg" alt="">
+                        <div>
+                            <span>
+                                Đặt lịch xét nghiệm bên này rất gọn, có ngày giờ cụ thể luôn lên là được xét nghiệm
+                                liền
+                                không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                                 không rườm rà gì mấy. An tâm đặt cho gia đình, có cả xét nghiệm tận nhà, không mất thời
+                                gian.
+                            </span>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <img src="./assets/imgs/d86082a5-b561-4904-a6c5-0122d8965ba9-nhan-nguyen.webp" alt="">
+                        <div class="name">
+                            <span>Nhân Nguyễn</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @include('components.navigation-swiper')
+</div>
---
+++ resources/views/components/lists/list3.blade.php
@@ -0,0 +1,68 @@
+@push('styles')
+    <link rel="stylesheet" href="./assets/css/home/lists/list3.css">
+@endpush
+<div class="home list3">
+    <x-title.title2 title="Thống kê" />
+
+    <div class="list-container">
+        <div class="list-item">
+            <div class="img">
+                <img src="./assets/imgs/2b42e50c-ad0f-4dcb-a45f-8e3c16f1e093-luotdatkham.webp" alt="">
+            </div>
+            <div class="content">
+                <div class="title">3.0M+</div>
+                <span>Lượt khám</span>
+            </div>
+        </div>
+
+        <div class="list-item">
+            <div class="img">
+                <img src="./assets/imgs/2b42e50c-ad0f-4dcb-a45f-8e3c16f1e093-luotdatkham.webp" alt="">
+            </div>
+            <div class="content">
+                <div class="title">3.0M+</div>
+                <span>Lượt khám</span>
+            </div>
+        </div>
+
+        <div class="list-item">
+            <div class="img">
+                <img src="./assets/imgs/2b42e50c-ad0f-4dcb-a45f-8e3c16f1e093-luotdatkham.webp" alt="">
+            </div>
+            <div class="content">
+                <div class="title">3.0M+</div>
+                <span>Lượt khám</span>
+            </div>
+        </div>
+
+        <div class="list-item">
+            <div class="img">
+                <img src="./assets/imgs/2b42e50c-ad0f-4dcb-a45f-8e3c16f1e093-luotdatkham.webp" alt="">
+            </div>
+            <div class="content">
+                <div class="title">3.0M+</div>
+                <span>Lượt khám</span>
+            </div>
+        </div>
+
+        <div class="list-item">
+            <div class="img">
+                <img src="./assets/imgs/2b42e50c-ad0f-4dcb-a45f-8e3c16f1e093-luotdatkham.webp" alt="">
+            </div>
+            <div class="content">
+                <div class="title">3.0M+</div>
+                <span>Lượt khám Lượt khám </span>
+            </div>
+        </div>
+
+        <div class="list-item">
+            <div class="img">
+                <img src="./assets/imgs/2b42e50c-ad0f-4dcb-a45f-8e3c16f1e093-luotdatkham.webp" alt="">
+            </div>
+            <div class="content">
+                <div class="title">3.0M+</div>
+                <span>Lượt khám</span>
+            </div>
+        </div>
+    </div>
+</div>
